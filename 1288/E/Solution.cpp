1edad0f24a473962826cfa8a779b8352fd1c7771
Claire <clairexy@google.com>
2020-07-04 19:43:08 +0000

    added comments to E

--- 1288/E/Solution.cpp
+++ 1288/E/Solution.cpp
@@ -19,6 +19,7 @@ vector<int> times;
 struct Solution {
   vector<int> bit;
   int B;
+  
   void update(int i, int v) { // update all the way up to the root
     i++;
     while (i <= B) {
@@ -26,15 +27,17 @@ struct Solution {
       i += i & (-i);
     }
   }
+  
   int get(int i) { // sum from i to the start of the array
     int res = 0;
     i++;
     while (i > 0) {
       res += bit[i];
-      i -= i & (-i);
+      i -= i & (-i);  // in this chain, all nodes represent exclusive sections
     }
     return res;
   }
+  
   string solve() {
     vector<int> mn(n), pos(n);
     iota(mn.begin(), mn.end(), 0); // fills in with original indexes
@@ -50,17 +53,17 @@ struct Solution {
     B = m + n;
     bit.resize(1 + B);
     for (int i = 0; i != n; ++i) {
-      pos[i] = m + i; //
+      pos[i] = m + i; // in the "n" section
+      // |--m---||-----n-----|
       update(pos[i], 1);
     }
 
     for (int i = 0; i != m; ++i) {
-      int p = times[i];
-      mx[p] = max(mx[p], get(pos[p] - 1));
-      
-      update(pos[p], -1);
+      int p = times[i]; // the person who messaged
+      mx[p] = max(mx[p], get(pos[p] - 1));  // query how many unique people in front of p
+      update(pos[p], -1); // remove them
       pos[p] = m - i - 1;
-      update(pos[p], 1);
+      update(pos[p], 1);  // add to front of the line
     }
 
     for (int i = 0; i != n; ++i) {
