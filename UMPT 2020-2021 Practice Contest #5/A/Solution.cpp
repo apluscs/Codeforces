84430ac3456f40bdacfb244097b2aa760c694e07
Claire <the.claire.yang@gmail.com>
2020-11-04 18:34:04 -0500

    umpt 4 and 5

--- UMPT 2020-2021 Practice Contest #5/A/Solution.cpp
+++ UMPT 2020-2021 Practice Contest #5/A/Solution.cpp
@@ -22,57 +22,44 @@
 #define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
 #define FORSQ(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a))
 #define FORC(a, b, c) for (char(a) = (b); (a) <= (c); ++(a))
-#define FOREACH(a, b) for (auto &(a) : (b))
+#define FOREACH(a, b) for (auto&(a) : (b))
 #define REP(i, n) FOR(i, 0, n)
 #define REPN(i, n) FORN(i, 1, n)
 
 using namespace std;
 const int mod = 1e9 + 7;
 
-int n, m;
-vector<vector<int>> adj;
-vector<int> edges;
+int n;
+vector<int> nums;
 struct Solution {
-  vector<int> vis, res;
   vector<int> solve() {
-    vis.resize(n);
-    res.resize(m, 1);
-    for (int i = 0; i != n; i++)
-      dfs(i);
-    return res;
-  }
-  void dfs(int curr) {  //goal is to make the last edge in the cycle = 2
-    if (vis[curr] == 2) return;
-    vis[curr] = 1;
-    for (auto e : adj[curr]) {
-      int to = edges[e];
-      if (vis[to] == 1) {
-        res[e] = 2;
-        continue;
+    vector<int> res(n);
+    REP(i, n) {
+      vector<bool> vis(n);
+      int j;
+      for (j = i; !vis[j]; vis[j] = true, j = nums[j]) {
+        // printf("j=%d\n", j);
       }
-      dfs(to);
+      res[i] = j + 1;
+      // cout << endl;
     }
-    vis[curr] = 2;
+    return res;
   }
 };
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  cin >> n >> m;
-  adj.clear();
-  adj.resize(n);
-  edges.resize(m);
-  int a, b;
-  REP(i, m) {
-    cin >> a >> b;
-    a--, b--;
-    adj[a].pb(i);  // index of outgoing edge
-    edges[i] = b;  // end point of that edge
+  cin >> n;
+  nums.resize(n);
+  REP(i, n) {
+    cin >> nums[i];
+    nums[i]--;
   }
   Solution test;
   auto res = test.solve();
-  cout << *max_element(res.begin(),res.end()) << endl;
-  for (auto r : res) cout << r << " ";
+  for (auto r : res) {
+    cout << r << " ";
+  }
   cout << endl;
 }
