e187b347ba58a0340359212ccc3d05a36e5a0018
Claire <the.claire.yang@gmail.com>
2021-02-26 15:15:50 -0500

    finally done

--- UMPT/Practice Contest 12/K/Solution.cpp
+++ UMPT/Practice Contest 12/K/Solution.cpp
@@ -38,63 +38,45 @@
 using namespace std;
 const int mod = 1e9 + 7;
 
-struct V {
-  int x, y1, y2;
-  V() {}
-  V(int x, int y1, int y2) : x(x), y1(y1), y2(y2) {}
-  void print() {
-    printf("x=%d, y1=%d, y2=%d\n", x, y1, y2);
-  }
-  bool operator<(const V& other) {  // by x
-    return x < other.x;
-  }
-};
-struct H {
-  int x1, y, x2;
-  H() {}
-  H(int x1, int y, int x2) : x1(x1), y(y), x2(x2) {}
-  bool operator<(const H& other) {  // by when they start
-    return x1 < other.x1;
-  }
-  void print() {
-    printf("x1=%d, x2=%d, y=%d\n", x1, x2, y);
-  }
-};
-
 int n, m, tree[10005];
-V vert[5001];
-H horz[5001];
+struct Node {
+  int x1, y1, x2, y2;
+
+  bool operator<(const Node& y) {  // sort by second x
+    return x1 < y.x1;
+  }
+};
+Node horz[5001], vert[5001];
 struct Solution {
   ll solve() {
     ll res = 0;
-    sort(vert, vert + n);
-    REP(i, n) {
-      vector<H> hs;
+    sort(horz + 1, horz + n + 1), sort(vert + 1, vert + m + 1);
+    for (int i = 1; i <= m; ++i) {  // right side
       memset(tree, 0, sizeof(tree));
-      REP(j, m) {  // find all horz that have right >= vert[i].x and add to hs and BIT for height range
-        if (horz[j].x2 >= vert[i].x) {
-          hs.push_back(horz[j]), update(horz[j].y, 1);
+      vector<int> q;
+      for (int j = 1; j <= n; ++j)
+        if (horz[j].x1 <= vert[i].x1 && horz[j].x2 >= vert[i].x1 && horz[j].y1 >= vert[i].y1 && horz[j].y2 <= vert[i].y2) {
+          // intersect with vert[i]
+          update(horz[j].y1, 1);  // add height of this horz
+          q.push_back(j);
         }
-      }
-      sort(hs.begin(), hs.end());
-      int h = hs.size() - 1;  // starts at H with largest left point
-      FORD(j, i - 1, 0) {
-        int crit_x = vert[j].x;
-        while (h != -1 && hs[h].x1 > crit_x) update(horz[h--].y, -1);  // ignore anything that isn't left enough to cover vert[j]
-        ll curr = query(min(vert[i].y2, vert[j].y2)) - query(max(vert[i].y1, vert[j].y1) - 1);
-        if (curr <= 1) continue;
-        res += curr * (curr - 1) / 2;
+      int tail = q.size() - 1;
+      for (int j = i - 1; j >= 1; --j) {                            // left side
+        for (; tail >= 0 && horz[q[tail]].x1 > vert[j].x1; --tail)  // k 单调递增
+          update(horz[q[tail]].y1, -1);                             //删去队列中不与 j 相交的竖线
+        int l = max(vert[i].y1, vert[j].y1) - 1, r = min(vert[i].y2, vert[j].y2);
+        if (l >= r) continue;           //两条线段有交
+        ll curr = query(r) - query(l);  //[l, r] 区间内的竖线能与 i,j 组成矩形
+        res += curr * (curr - 1) / 2;   //统计答案
       }
     }
     return res;
   }
   void update(int v, int d) {
-    v += 5001;
     for (int i = v; i <= 10005; i += (i & -i)) tree[i] += d;
   }
   int query(int v) {
     int res = 0;
-    v += 5001;
     for (int i = v; i; i -= (i & -i)) res += tree[i];
     return res;
   }
@@ -105,13 +87,13 @@ int main() {
   cin.tie(0);
   int T, x1, y1, x2, y2;
   cin >> T, n = m = 0;
-  REP(i, T) {
+  while (T--) {
     cin >> x1 >> y1 >> x2 >> y2;
-    if (x1 == x2) {  // vert
-      vert[n++] = V(x1, min(y1, y2), max(y1, y2));
-    } else {
-      horz[m++] = H(min(x1, x2), y1, max(x1, x2));
-    }
+    x1 += 5001, y1 += 5001, x2 += 5001, y2 += 5001;  //防止出现负数
+    if (y1 == y2)
+      horz[++n] = (Node){min(x1, x2), y1, max(x1, x2), y2};
+    else
+      vert[++m] = (Node){x1, min(y1, y2), x2, max(y1, y2)};
   }
   Solution test;
   cout << test.solve() << endl;
